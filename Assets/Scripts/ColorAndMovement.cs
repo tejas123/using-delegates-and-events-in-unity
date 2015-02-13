beb4c58ff4eab75cfc3842b596e074a9d5d9dc8d
Bharat <bharatappguru@gmail.com>
2015-02-13 17:39:01 +0530

    Using Delegates and Events in Unity

---
+++ Assets/Scripts/ColorAndMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorAndMovement : MonoBehaviour {
+
+	public Text textposition,textcolor;
+
+	//add the methods to events 
+	void OnEnable () {
+		DelegateHandler.Click += ChangePosition;
+		DelegateHandler.Click += ChangeColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//move the ball to right 
+	void ChangePosition()
+	{
+		textposition.text = "Position Changed";
+		transform.position = new Vector2 (transform.position.x + 1, transform.position.y);
+	}
+
+	//change the color of ball and text
+	void ChangeColor()
+	{
+		Color color=new Color (Random.value, Random.value, Random.value);
+		textcolor.text = "Color Changed";
+
+		//set the color on ball and text
+		transform.renderer.material.color = color;
+		textcolor.color=color;
+	}
+
+	//remove the methods added before
+	void OnDisable()
+	{
+		DelegateHandler.Click -= ChangePosition;
+		DelegateHandler.Click -= ChangeColor;
+	}
+}
---
+++ Assets/Scripts/DelegateHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateHandler : MonoBehaviour {
+
+
+	//delegate <return-type> <delegate-name> (<Parameters list>)
+
+	public delegate void ButtonClick();
+
+
+	//event <delegate-name> <event-name>
+
+	public static event ButtonClick Click;
+
+
+	//On Button Click the event named Click can be called 
+	public void OnButtonClick()
+	{
+		//call the event
+		Click ();
+	}
+}
